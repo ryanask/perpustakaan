975d94bd663e6c5cc72af43dac571a906bf1cf3a
irvan novianto <v4nt4t@gmail.com>
2015-08-12 13:08:47 +0700

    commit irvan 1

--- perpustakaan/src/main/java/id/co/askrindo/perpustakaan/entity/Rak.java
+++ perpustakaan/src/main/java/id/co/askrindo/perpustakaan/entity/Rak.java
@@ -2,5 +2,29 @@ package id.co.askrindo.perpustakaan.entity;
 
 public class Rak {
 	Integer id;
+	Integer namaRak;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNamaRak() {
+        return namaRak;
+    }
+
+    public void setNamaRak(Integer namaRak) {
+        this.namaRak = namaRak;
+    }
+
+    @Override
+    public String toString() {
+        return "Rak{" + "id=" + id + ", namaRak=" + namaRak + '}';
+    }
+        
+        
 	
 }
